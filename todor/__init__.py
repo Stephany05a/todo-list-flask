7059ee7d5620cd42272828d66ef525bc94385b9c
Stephany05a <al222311543@gmail.com>
2026-01-17 02:34:01 -0600

    Update __init__.py
    Agrego estructura inicial del proyecto Flask todo-list

--- todor/__init__.py
+++ todor/__init__.py
@@ -1,1 +1,12 @@
+from flask import Flask
 
+def create_app():
+    app = Flask(__name__)
+
+    from .todo import todo
+    from .auth import auth
+
+    app.register_blueprint(todo)
+    app.register_blueprint(auth)
+
+    return app
